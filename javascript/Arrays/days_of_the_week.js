123578151d9ef0e616acccf7ef6c9031800883e8
cassilam <rm.cassilam@gmail.com>
2017-12-25 18:22:27 -0800

    Added Array sample code

---
+++ javascript/Arrays/days_of_the_week.js
@@ -0,0 +1,9 @@
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+console.log("The first day of the week is " + days[0]);
+console.log("The second day of the week is " + days[1]);
+console.log("The third day of the week is " + days[2]);
+console.log("The fourth day of the week is " + days[3]);
+console.log("The fifth day of the week is " + days[4]);
+console.log("The sixth day of the week is " + days[5]);
+console.log("The seventh day of the week is " + days[6]);
